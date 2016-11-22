3446e2887c2b922393d5b69b6915f2d82a9e8c5e
Davi Nascimento <davi-nasc@hotmail.com>
2016-11-21 23:10:38 -0300

    Removido scaffold de Users, instalado Devise para controle de login

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '5.0'
 # Use sqlite3 as the database for Active Record
 
+gem 'devise'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
-class User < ActiveRecord::Base
-	has_many :registrations
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :registrantions
 end
