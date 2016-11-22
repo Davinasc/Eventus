c6de4275e1b6502b56fe868ed0f9658b0d5581ea
Davi Nascimento <davi-nasc@hotmail.com>
2016-11-22 16:56:07 -0300

    Drop user table for fix a duplicate problme in heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122193810) do
+ActiveRecord::Schema.define(version: 20161122195506) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -39,15 +39,6 @@ ActiveRecord::Schema.define(version: 20161122193810) do
     t.index ["activity_id"], name: "index_registrations_on_activity_id"
     t.index ["event_id"], name: "index_registrations_on_event_id"
     t.index ["user_id"], name: "index_registrations_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
