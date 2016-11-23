e436e4801c80be564e2617aecb4f90d9eb1884a1
Davi Nascimento <davi-nasc@hotmail.com>
2016-11-23 18:14:53 -0300

    Finalizado singup de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get  '/signup', to: 'users#new'
-
+  post '/signup',  to: 'users#create'
+  resources :users
   get 'welcome/index'
 
   resources :events do 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122195506) do
+ActiveRecord::Schema.define(version: 20161118213607) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
